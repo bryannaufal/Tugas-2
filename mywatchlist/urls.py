1e11b645014b38d5697298cbc152fb56348bf071
RahBry <bryan.naufal1@gmail.com>
2022-09-22 06:33:07 +0700

    try

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from mywatchlist.views import show_mywatchlist
-from mywatchlist.views import show_xml #sesuaikan dengan nama fungsi yang dibuat
+from mywatchlist.views import show_xml
 from mywatchlist.views import show_json #sesuaikan dengan nama fungsi yang dibuat
 from mywatchlist.views import show_json_by_id
 from mywatchlist.views import show_xml_by_id
